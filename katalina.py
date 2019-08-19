5504768f664f6cd3944505d478d1e3ee37cdf766
adolfo <zayronxio@gmail.com>
2019-08-19 02:31:25 -0500

    1

--- katalina.py
+++ katalina.py
@@ -17,7 +17,7 @@ class Handler:
         commands.getstatusoutput('xfconf-query -c xsettings -p /Net/IconThemeName -s Os-Catalina-Night && xfconf-query -c xsettings -p /Net/ThemeName -s Os-Catalina-Gtk-night && xfconf-query -c xfwm4 -p /general/theme  -s Os-Catalina-Gtk-night && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $HOME/.local/share/wallpapers/Wallnight.jpg && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVGA-1/workspace0/last-image -s $HOME/.local/share/wallpapers/Wallnight.jpg && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor1/image-path -s $HOME/.local/share/wallpapers/Wallnight.jpg && xfdesktop -R')
 
 builder = Gtk.Builder()
-builder.add_from_file("katalina1.glade")
+builder.add_from_file("katalina1.ui")
 builder.connect_signals(Handler())
 
 window = builder.get_object("window_general")
